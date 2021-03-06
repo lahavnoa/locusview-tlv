0f1335a3c0b5d72b47f86cb0338379720603068c
shlomi <shlomi.lahav@locusview.com>
2021-03-06 13:04:11 +0200

    update envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBZi2MM5Y2daE1LSfX-bDrc_Ykkp1ws2io',
+    authDomain: 'locusview-tlv.firebaseapp.com',
+    databaseURL: 'https://locusview-tlv-default-rtdb.firebaseio.com',
+    projectId: 'locusview-tlv',
+    storageBucket: 'locusview-tlv.appspot.com',
+    messagingSenderId: '341197574668',
+    appId: '1:341197574668:web:3271d5e28460af0b665429',
+    measurementId: 'G-ZM7ZEL8D6F'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBZi2MM5Y2daE1LSfX-bDrc_Ykkp1ws2io',
+    authDomain: 'locusview-tlv.firebaseapp.com',
+    databaseURL: 'https://locusview-tlv-default-rtdb.firebaseio.com',
+    projectId: 'locusview-tlv',
+    storageBucket: 'locusview-tlv.appspot.com',
+    messagingSenderId: '341197574668',
+    appId: '1:341197574668:web:3271d5e28460af0b665429',
+    measurementId: 'G-ZM7ZEL8D6F'
+  }
 };
 
 /*
