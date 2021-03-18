5461721b9ef7eac7cf5da8808f7418c078d20017
shlomi <shlomi.lahav@locusview.com>
2021-03-18 16:13:15 +0200

    Jobs API and update locusviewers. Remove firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {environment} from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { LvHeaderComponent } from './header/header.component';
 import { LvPropertiesComponent } from './lv-properties/lv-properties.component';
 import { LvLocusviewerComponent } from './lv-locusviewer/lv-locusviewer.component';
@@ -14,6 +11,7 @@ import { LvDayToDayComponent } from './lv-day-to-day/lv-day-to-day.component';
 import { LvInMediaComponent } from './lv-in-media/lv-in-media.component';
 import { LvFooterComponent } from './lv-footer/lv-footer.component';
 import {IvyCarouselModule} from 'angular-responsive-carousel';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -30,9 +28,8 @@ import {IvyCarouselModule} from 'angular-responsive-carousel';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
-    IvyCarouselModule
+    IvyCarouselModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/lv-jobs/lv-jobs.component.ts
+++ src/app/lv-jobs/lv-jobs.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+export interface IJob {
+  company_name: string;
+  department: string;
+  email: string;
+  employment_type: string;
+  experience_level: string;
+  internal_use_custom_id: string;
+  location: {
+    arrival_instructions: string;
+    city: string;
+    country: string;
+    is_remote: boolean;
+    location_uid: string;
+    name: string;
+    postal_code: string;
+    state: string;
+    street_name: string;
+    street_number: number;
+    timezone: string;
+  };
+  city: string;
+  country: string;
+  is_remote: boolean;
+  location_uid: string;
+  name: string;
+  postal_code: string;
+  state: string;
+  street_name: string;
+  street_number: number;
+  timezone: string;
+  picture_url: string;
+  position_url: string;
+  time_updated: string;
+  uid: string;
+  url_active_page: string;
+  url_comeet_hosted_page: string;
+  url_detected_page: string;
+}
 
 @Component({
   selector: 'lv-jobs',
   templateUrl: './lv-jobs.component.html',
-  styleUrls: ['./lv-jobs.component.scss']
+  styleUrls: ['./lv-jobs.component.scss'],
+  providers: [HttpClient]
 })
 export class LvJobsComponent implements OnInit {
+  public jobs: IJob[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {
   }
 
+  ngOnInit(): void {
+    this.updateJobs();
+  }
+
+  private async updateJobs() {
+    this.http.get(
+      'https://www.comeet.co/careers-api/2.0/company/25.002/positions?token=52219AA23EE148802E32F66148829100').subscribe((jobs) => {
+      const results = jobs as IJob[];
+      if (results?.length > 0) {
+        this.jobs = results.filter(job => job.location.country === 'IL');
+      }
+    });
+  }
 }
